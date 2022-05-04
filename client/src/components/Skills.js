16ec60da82ebc4368937b0f714212075aeeb4d8e
Maia I Elias <maiaineselias@gmail.com>
2022-05-03 21:43:34 -0300

    (skill) spacing

--- client/src/components/Skills.js
+++ client/src/components/Skills.js
@@ -10,32 +10,32 @@ import mysql from '../images/mysql.png'
 
 export const Skills = () => {
     return (
-        <div className='container col-lg-8 d-flex align-items-center justify-content-evenly'>
-            <img srcSet={`${css}  320w`}
+        <div className='container col-lg-8 d-flex align-items-center justify-content-evenly flex-wrap skillChild'>
+            <img className="skill" srcSet={`${css}  320w`}
             sizes='(max-width: 768px) 50px,
                     80px' src={css} alt="css logo"/>
 
-            <img  srcSet={`${html5}  320w`}
+            <img className="skill"  srcSet={`${html5}  320w`}
             sizes='(max-width: 768px) 50px,
                     80px' src={html5}  alt="html5 logo"/>
 
-            <img  srcSet={`${js}  320w`}
+            <img className="skill"  srcSet={`${js}  320w`}
             sizes='(max-width: 768px) 50px,
                     80px' src={js} alt="js logo"/>
 
-            <img  srcSet={`${nodejs}  320w`}
+            <img className="skill"  srcSet={`${nodejs}  320w`}
             sizes='(max-width: 768px) 50px,
                     80px' src={nodejs} alt="nodejs logo"/>
 
-            <img  srcSet={`${reactjs}  320w`}
+            <img className="skill"  srcSet={`${reactjs}  320w`}
             sizes='(max-width: 768px) 50px,
                     80px' src={reactjs} alt="reactjs logo"/>
 
-            <img  srcSet={`${mongodb}  320w`}
+            <img className="skill"  srcSet={`${mongodb}  320w`}
             sizes='(max-width: 768px) 50px,
                     80px' src={mongodb} alt="mongodb logo"/>
 
-            <img  srcSet={`${mysql}  320w`}
+            <img className="skill"  srcSet={`${mysql}  320w`}
             sizes='(max-width: 768px) 50px,
                     80px' src={mysql} alt="mysql logo"/>
         </div>
